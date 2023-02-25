cc5545d3524e2d030dfe0967706217626b7a6719
mhoihean <nicholas.mak@u.nus.edu>
2023-02-25 16:30:55 +0800

    added policylist token

--- src/scenes/policy/PolicyList.js
+++ src/scenes/policy/PolicyList.js
@@ -7,18 +7,11 @@ import { Box, Button, TextField } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 import { DataGrid } from "@mui/x-data-grid";
-
+import { useAuth } from "../../Auth";
 
 const PolicyList = () => {
   const [policies, setPolicy] = useState([])
-  const [email, setEmail] = useState([])
-
-  /*
-  useEffect(() => {
-    setEmail(reactLocalStorage.getObject('user').email);
-    getUsers();
-  }, []);
-  */
+  const token = useAuth();
 
   useEffect(() => {
     getPolicies();
@@ -26,7 +19,7 @@ const PolicyList = () => {
 
   const getPolicies = async () => {
     // user = reactLocalStorage.getObject('user').userID
-    const response = await axios.get("http://localhost:5001/getPolicies", { params: { employeeId: 58001003 } });
+    const response = await axios.get("http://localhost:5001/getPolicies", {headers: { Authorization: `Bearer ${token}` }});
     // const response = await axios.get("http://localhost:5001/getPolicies", { params: { employeeId: user.userID } });
     setPolicy(response.data);
   };
